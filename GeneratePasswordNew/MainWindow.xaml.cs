d9416878c1f59fe7e218308fcc62f3b5a7c1b6c1
d1choff <a.cozulin@yandex.ru>
2021-12-16 14:21:10 +0900

    Fix gen

--- GeneratePasswordNew/MainWindow.xaml.cs
+++ GeneratePasswordNew/MainWindow.xaml.cs
@@ -29,7 +29,7 @@ namespace GeneratePasswordNew
         {
             string result = "";
             Random rnd = new Random();
-            string[] abc = new string[] { "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")" };
+            string[] abc = new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0","q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M", "!", "@", "#", "$", "%", "^", "&", "*", "("};
             string Num = "0123456789";
             int Numbers = Convert.ToInt32(tbLengthPass.Text);
 
